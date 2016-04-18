2af3c13930c83b86c61bde7d24acfdb22d4c38a0
James Sullivan <james@scelos.com>
2016-04-17 23:48:45 -0400

    Switched to React and Added a Leaflet pane

---
+++ client/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MapView from './js/map-view';
+
+ReactDOM.render(
+    <MapView />,
+    document.getElementById('mapView')
+);
---
+++ client/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    entry: {
+        default: [
+        './main.js'
+        'webpack-dev-server/client?http://localhost:8080'];
+    },
+    output: {
+        filename: 'bundle.js'
+    }
+};
