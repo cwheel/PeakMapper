8f86b73f8c6ab404c161133bde96c16059675e0c
Cameron Wheeler <cameron@scelos.com>
2016-04-18 11:07:17 -0400

    Sidebar animation

--- client/components/sidebarComponent.jsx
+++ client/components/sidebarComponent.jsx
@@ -1,27 +1,49 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {Motion, spring} from 'react-motion';
 
 class SidebarComponent extends React.Component {
 	constructor() {
 		super();
 		
 		this.state = {
+			visible: true
 		};
+
+		this.disclose = this.disclose.bind(this);
 	}
 
+	
+
 	render() {
-		const sidebarStyle = {
-			zIndex: 1000,
+		const barStyle = {
 			position: 'absolute',
-			height: '100%',
-			width: '250px',
-			background: '#fff',
-			boxShadow: '6px 0px 77px -25px rgba(0,0,0,0.75)'
+			top: '10px',
+			right: '10px',
+			fontSize: '25px',
+			color: '#444',
+			cursor: 'pointer'
 		};
 
+		const slide = 210;
+
 		return (
-			<div style={sidebarStyle}>hello world</div>
+			<div>
+				<Motion style={{x: spring(this.state.visible ? 0 : -slide), alpha: spring(this.state.visible ? 1 : 0)}}>
+					{({x, alpha}) => 
+						<div className="sidebar" style={{transform: `translate3d(${x}px, 0, 0)`, background: `rgba(255,255,255,${alpha})`, boxShadow: `6px 0px 77px -25px rgba(0,0,0,${alpha})`}}>
+							<i className="fa fa-bars" aria-hidden="true" style={barStyle} onClick={this.disclose}></i>
+							hello world
+						</div>
+					}
+				</Motion>
+			</div>
 		);
+	}
+
+
+	disclose() {
+		this.setState({visible: !this.state.visible});
 	}
 }
 
