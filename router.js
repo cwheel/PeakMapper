273a00e6352d29b175e8c5a82ca18b72f41e4537
James Sullivan <james@scelos.com>
2016-04-19 00:45:54 -0400

    Started Search augmentation

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var request = require('request');
+var config = require('./config.json')
 module.exports = function(app) {
 	router.use(function(req, res, next) {
 		console.log(req.method.toUppercase() + " " + req.req.originalUrl);
@@ -8,6 +9,16 @@ module.exports = function(app) {
 	});
 
 	app.use('/api', router);
+
+	app.get('/nominatim', function (req,res){
+		console.log(req.query);
+		var query = ""
+		for(var key in req.query){
+			query += key + "=" + req.query[key] + "&";
+		}
+		query = "?" + query + "key=" + config.mapquestKey;
+		res.send(query);
+	})
 
 	router.route('/trailreport');
 }
